5fbb371a0113be16e0a7f8663ac9e2234345245c
sosagz <159752173+sosagz@users.noreply.github.com>
2024-07-31 03:58:21 -0400

    Completed project #2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let string1 = "";
+    for (i = 0; i < num; i++) {
+        string1+= string;
+    }
+    return string1
 };
 
 // Do not edit below this line
